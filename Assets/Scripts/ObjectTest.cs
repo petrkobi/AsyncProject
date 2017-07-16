4d0ad52fddf3fd119d9dad556aaa3251565181f7
Petr Kobilak <petr.kobi@gmail.com>
2017-07-17 01:11:24 +0200

    * test object -> type to int or float

---
+++ Assets/Scripts/ObjectTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTest : MonoBehaviour {
+
+	private object i_obj = 100;
+	private object f_obj = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (i_obj.ToString ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			
+			int i = 0;
+			float f = 0;
+			Debug.Log (i_obj.GetType ());
+
+			i = (int)i_obj;
+			Debug.Log (i);
+
+			f = (float)f_obj;
+			Debug.Log (f);
+		}
+	}
+}
